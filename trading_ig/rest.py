734ed1198ace2ed85df64db3b410c3ed81784c87
hklang10 <32039983+hklang10@users.noreply.github.com>
2017-09-18 06:38:51 +0100

    When a session is created, raise exception if HTTP error response (#41) (#60)
    * When a session is created, raise exception if HTTP error response (#41)
    
    * simplify if statement.

--- trading_ig/rest.py
+++ trading_ig/rest.py
@@ -78,6 +78,9 @@ class IGSessionCRUD(object):
         response = session.post(url,
                                 data=json.dumps(params),
                                 headers=self.HEADERS['BASIC'])
+        if not response.ok:
+            raise(Exception("HTTP status code %s %s " %
+                            (response.status_code, response.text)))
         self._set_headers(response.headers, True)
         self.create = self._create_logged_in
         return response
