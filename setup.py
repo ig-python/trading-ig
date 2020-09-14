5cf594b2d9cc5df626022ffb2a19652f07bd2348
Andy Geach <andy@bugorfeature.net>
2020-09-14 08:23:49 +0100

    Adding crypto package to dependencies. Implements PR #124 from upstream repo

--- setup.py
+++ setup.py
@@ -74,6 +74,7 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
     install_requires=[
+        "pycryptodome",
         "pandas",
         "requests",
         "requests-cache",
