d592c7000957ff2931a01cb5d2153ec24be954a1
Andy Geach <andy@bugorfeature.net>
2020-10-11 11:30:10 +0100

    Fix for a bug in 'expand-columns()' that would cause an error if values were missing/null

---
+++ tests/test_dealing.py
@@ -0,0 +1,44 @@
+from trading_ig.rest import IGService
+import responses
+import json
+import pandas as pd
+
+"""
+unit tests for dealing methods
+"""
+
+
+class TestDealing:
+
+    # login v1
+
+    @responses.activate
+    def test_workingorders_happy(self):
+
+        with open('tests/data/workingorders.json', 'r') as file:
+            response_body = json.loads(file.read())
+
+        responses.add(responses.GET, 'https://demo-api.ig.com/gateway/deal/workingorders',
+                      headers={'CST': 'abc123', 'X-SECURITY-TOKEN': 'xyz987'},
+                      json=response_body,
+                      status=200)
+
+        ig_service = IGService('username', 'password', 'api_key', 'DEMO')
+        ig_service.crud_session.HEADERS["LOGGED_IN"] = {}
+        result = ig_service.fetch_working_orders()
+
+        pd.set_option('display.max_columns', 50)
+        print(result)
+
+        assert isinstance(result, pd.DataFrame)
+        assert result.iloc[0]['instrumentName'] == 'Spot Gold'
+        assert result.iloc[0]['exchangeId'] == 'FX_C_GCSI_ST'
+        assert result.iloc[0]['marketStatus'] == 'EDITS_ONLY'
+        assert result.iloc[0]['level'] == 2000.0
+        assert result.iloc[0]['epic'] == 'CS.D.CFDGOLD.CFDGC.IP'
+        assert result.iloc[0]['currencyCode'] == 'USD'
+
+
+
+
+
