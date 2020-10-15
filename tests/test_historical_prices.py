cc6f36d48ae168e587142e0528e6e1eb2866ed32
Andy Geach <andy@bugorfeature.net>
2020-10-15 06:53:23 +0100

    Test for dates out of order

--- tests/test_historical_prices.py
+++ tests/test_historical_prices.py
@@ -12,16 +12,6 @@ unit tests for historical prices methods
 
 class TestHistoricalPrices:
 
-
-    epic_map = {
-        'GOLD': 'MT.D.GC.Month2.IP',
-        'CORN': 'CO.D.C.Month1.IP',
-        'EUROSTX': 'IX.D.STXE.MONTH3.IP',
-        'SP500': 'IX.D.SPTRD.MONTH1.IP',
-        'AUD': 'CF.D.AUD.DEC.IP', # 'CF.D.AUD.MAR.IP'
-    }
-
-
     @responses.activate
     def test_historical_prices_v3_defaults_happy(self):
 
@@ -30,7 +20,8 @@ class TestHistoricalPrices:
         with open('tests/data/historic_prices.json', 'r') as file:
             response_body = json.loads(file.read())
 
-        responses.add(responses.GET, 'https://demo-api.ig.com/gateway/deal/prices/MT.D.GC.Month2.IP',
+        responses.add(responses.GET,
+                      'https://demo-api.ig.com/gateway/deal/prices/MT.D.GC.Month2.IP',
                       headers={'CST': 'abc123', 'X-SECURITY-TOKEN': 'xyz987'},
                       json=response_body,
                       status=200)
@@ -52,7 +43,6 @@ class TestHistoricalPrices:
         prices['delta'] = (prices['tvalue'] - prices['tvalue'].shift())
         assert any(prices["delta"].dropna() == datetime.timedelta(minutes=1))
 
-
     @responses.activate
     def test_historical_prices_v3_datetime_happy(self):
 
@@ -61,7 +51,8 @@ class TestHistoricalPrices:
         with open('tests/data/historic_prices_dates.json', 'r') as file:
             response_body = json.loads(file.read())
 
-        responses.add(responses.GET, 'https://demo-api.ig.com/gateway/deal/prices/MT.D.GC.Month2.IP',
+        responses.add(responses.GET,
+                      'https://demo-api.ig.com/gateway/deal/prices/MT.D.GC.Month2.IP',
                       match_querystring=False,
                       headers={'CST': 'abc123', 'X-SECURITY-TOKEN': 'xyz987'},
                       json=response_body,
@@ -69,10 +60,11 @@ class TestHistoricalPrices:
 
         ig_service = IGService('username', 'password', 'api_key', 'DEMO')
         ig_service.crud_session.HEADERS["LOGGED_IN"] = {}
-        result = ig_service.fetch_historical_prices_by_epic(epic='MT.D.GC.Month2.IP',
-                                                            resolution='D',
-                                                            start_date='2020-09-01T00:00:00',
-                                                            end_date='2020-09-04T23:59:59')
+        result = ig_service.fetch_historical_prices_by_epic(
+            epic='MT.D.GC.Month2.IP',
+            resolution='D',
+            start_date='2020-09-01T00:00:00',
+            end_date='2020-09-04T23:59:59')
 
         prices = result['prices']
         assert isinstance(result, dict)
@@ -92,7 +84,6 @@ class TestHistoricalPrices:
         assert result['metadata']['pageData']['pageNumber'] == 1
         assert result['metadata']['pageData']['totalPages'] == 1
 
-
     @responses.activate
     def test_historical_prices_v3_num_points_happy(self):
 
@@ -101,7 +92,8 @@ class TestHistoricalPrices:
         with open('tests/data/historic_prices_num_points.json', 'r') as file:
             response_body = json.loads(file.read())
 
-        responses.add(responses.GET, 'https://demo-api.ig.com/gateway/deal/prices/MT.D.GC.Month2.IP',
+        responses.add(responses.GET,
+                      'https://demo-api.ig.com/gateway/deal/prices/MT.D.GC.Month2.IP',
                       match_querystring=False,
                       headers={'CST': 'abc123', 'X-SECURITY-TOKEN': 'xyz987'},
                       json=response_body,
@@ -126,13 +118,13 @@ class TestHistoricalPrices:
         prices['delta'] = (prices['tvalue'] - prices['tvalue'].shift())
         assert any(prices["delta"].dropna() == datetime.timedelta(weeks=1))
 
-
     @responses.activate
     def test_historical_prices_v3_bad_epic(self):
 
         # fetch_historical_prices v3 - bad epic
 
-        responses.add(responses.GET, 'https://demo-api.ig.com/gateway/deal/prices/MT.D.XX.Month1.IP',
+        responses.add(responses.GET,
+                      'https://demo-api.ig.com/gateway/deal/prices/MT.D.X.Month1.IP',
                       headers={'CST': 'abc123', 'X-SECURITY-TOKEN': 'xyz987'},
                       json={'errorCode': 'error.error.price-history.io-error'},
                       status=404)
@@ -140,7 +132,7 @@ class TestHistoricalPrices:
         with pytest.raises(Exception):
             ig_service = IGService('username', 'password', 'api_key', 'DEMO')
             ig_service.crud_session.HEADERS["LOGGED_IN"] = {}
-            result = ig_service.fetch_historical_prices_by_epic(epic='MT.D.XX.Month1.IP')
+            result = ig_service.fetch_historical_prices_by_epic(epic='MT.D.X.Month1.IP')
             assert result['errorCode'] == 'error.error.price-history.io-error'
 
     @responses.activate
@@ -148,18 +140,40 @@ class TestHistoricalPrices:
 
         # fetch_historical_prices v3 - bad date format
 
-        responses.add(responses.GET, 'https://demo-api.ig.com/gateway/deal/prices/MT.D.XX.Month1.IP',
-                      headers={'CST': 'abc123', 'X-SECURITY-TOKEN': 'xyz987'},
-                      json={'errorCode': 'Unable to parse datetime=2020/09/01T00:00:00'},
-                      status=400)
-
-        '{"errorCode":"Unable to parse datetime=2020/09/01T00:00:00"}'
+        responses.add(
+            responses.GET,
+            'https://demo-api.ig.com/gateway/deal/prices/MT.D.XX.Month1.IP',
+            headers={'CST': 'abc123', 'X-SECURITY-TOKEN': 'xyz987'},
+            json={'errorCode': 'Unable to parse datetime=2020/09/01T00:00:00'},
+            status=400)
 
         with pytest.raises(Exception):
             ig_service = IGService('username', 'password', 'api_key', 'DEMO')
             ig_service.crud_session.HEADERS["LOGGED_IN"] = {}
-            result = ig_service.fetch_historical_prices_by_epic(epic='MT.D.GC.Month2.IP',
-                                                                        resolution='D',
-                                                                        start_date='2020/09/01T00:00:00',
-                                                                        end_date='2020-09-04T23:59:59')
+            result = ig_service.fetch_historical_prices_by_epic(
+                epic='MT.D.GC.Month2.IP',
+                resolution='D',
+                start_date='2020/09/01T00:00:00',
+                end_date='2020-09-04T23:59:59')
             assert result['errorCode'].startswith('Unable to parse datetime')
+
+    @responses.activate
+    def test_historical_prices_v3_bad_date_order(self):
+
+        # fetch_historical_prices v3 - bad date order
+
+        responses.add(responses.GET,
+                      'https://demo-api.ig.com/gateway/deal/prices/MT.D.XX.Month1.IP',
+                      headers={'CST': 'abc123', 'X-SECURITY-TOKEN': 'xyz987'},
+                      json={"errorCode": "error.invalid.daterange"},
+                      status=400)
+
+        with pytest.raises(Exception):
+            ig_service = IGService('username', 'password', 'api_key', 'DEMO')
+            ig_service.crud_session.HEADERS["LOGGED_IN"] = {}
+            result = ig_service.fetch_historical_prices_by_epic(
+                epic='MT.D.GC.Month2.IP',
+                resolution='D',
+                start_date='2020-09-04T23:59:59',
+                end_date='2020/09/01T00:00:00')
+            assert result['errorCode'] == 'error.invalid.daterange'
