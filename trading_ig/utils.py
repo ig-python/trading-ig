bfa0f52cb103efdafe4baa2a6256bc35b558146f
Andy Geach <andy@bugorfeature.net>
2021-07-07 08:37:11 +0100

    linting

--- trading_ig/utils.py
+++ trading_ig/utils.py
@@ -110,7 +110,7 @@ def print_full(x):
     pd.set_option('display.max_rows', None)
     pd.set_option('display.max_columns', None)
     pd.set_option('display.width', 2000)
-    #pd.set_option('display.float_format', '{:20,.2f}'.format)
+    # pd.set_option('display.float_format', '{:20,.2f}'.format)
     pd.set_option('display.max_colwidth', None)
     print(x)
     pd.reset_option('display.max_rows')
