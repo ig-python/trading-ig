4b6e8029526f60b588769d35272e15323515460a
Andy Geach <andy@bugorfeature.net>
2021-07-07 08:34:20 +0100

    new util function to display a full dataframe

--- trading_ig/utils.py
+++ trading_ig/utils.py
@@ -101,3 +101,20 @@ def remove(cache):
         os.remove(filename)
     except Exception:
         pass
+
+
+def print_full(x):
+    """
+    Prints out a full data frame, no column hiding
+    """
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', 2000)
+    #pd.set_option('display.float_format', '{:20,.2f}'.format)
+    pd.set_option('display.max_colwidth', None)
+    print(x)
+    pd.reset_option('display.max_rows')
+    pd.reset_option('display.max_columns')
+    pd.reset_option('display.width')
+    pd.reset_option('display.float_format')
+    pd.reset_option('display.max_colwidth')
