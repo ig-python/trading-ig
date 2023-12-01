5ef705b0664e3cad3f11cbebff52cec2ea310547
Andy Geach <andy@bugorfeature.net>
2023-12-01 11:23:56 +0000

    include invalid token in tests

--- tests/retry_test.py
+++ tests/retry_test.py
@@ -1,4 +1,4 @@
-from trading_ig.rest import IGService, ApiExceededException
+from trading_ig.rest import IGService, ApiExceededException, TokenInvalidException
 import responses
 from responses import Response
 import json
@@ -11,7 +11,7 @@ class TestRetry:
     # test_retry
 
     @responses.activate
-    def test_retry(self):
+    def test_exceed_retry(self):
         with open("tests/data/accounts_balances.json", "r") as file:
             response_body = json.loads(file.read())
 
@@ -56,5 +56,96 @@ class TestRetry:
         result = ig_service.fetch_accounts()
 
         assert responses.assert_call_count(url, 4) is True
+        assert isinstance(result, pd.DataFrame)
+        assert result.iloc[0]["accountId"] == "XYZ987"
+
+    @responses.activate
+    def test_token_retry(self):
+        with open("tests/data/accounts_balances.json", "r") as file:
+            response_body = json.loads(file.read())
+
+        url = "https://demo-api.ig.com/gateway/deal/accounts"
+        headers = {"CST": "abc123", "X-SECURITY-TOKEN": "xyz987"}
+        client_token_error = {"errorCode": "error.security.client-token-invalid"}
+        oauth_token_error = {"errorCode": "error.security.oauth-token-invalid"}
+
+        responses.add(
+            Response(
+                method="GET", url=url, headers={}, json=client_token_error, status=403
+            )
+        )
+        responses.add(
+            Response(
+                method="GET", url=url, headers={}, json=oauth_token_error, status=403
+            )
+        )
+        responses.add(
+            Response(
+                method="GET", url=url, headers={}, json=client_token_error, status=403
+            )
+        )
+        responses.add(
+            Response(
+                method="GET", url=url, headers=headers, json=response_body, status=200
+            )
+        )
+
+        ig_service = IGService(
+            "username",
+            "password",
+            "api_key",
+            "DEMO",
+            retryer=Retrying(
+                wait=tenacity.wait_exponential(),
+                retry=tenacity.retry_if_exception_type(TokenInvalidException),
+            ),
+        )
+
+        result = ig_service.fetch_accounts()
+
+        assert responses.assert_call_count(url, 4) is True
+        assert isinstance(result, pd.DataFrame)
+        assert result.iloc[0]["accountId"] == "XYZ987"
+
+    @responses.activate
+    def test_all_retry(self):
+        with open("tests/data/accounts_balances.json", "r") as file:
+            response_body = json.loads(file.read())
+
+        url = "https://demo-api.ig.com/gateway/deal/accounts"
+        headers = {"CST": "abc123", "X-SECURITY-TOKEN": "xyz987"}
+        client_token_error = {"errorCode": "error.security.client-token-invalid"}
+        api_exceeded = {"errorCode": "error.public-api.exceeded-api-key-allowance"}
+
+        responses.add(
+            Response(
+                method="GET", url=url, headers={}, json=client_token_error, status=403
+            )
+        )
+        responses.add(
+            Response(method="GET", url=url, headers={}, json=api_exceeded, status=403)
+        )
+        responses.add(
+            Response(
+                method="GET", url=url, headers=headers, json=response_body, status=200
+            )
+        )
+
+        ig_service = IGService(
+            "username",
+            "password",
+            "api_key",
+            "DEMO",
+            retryer=Retrying(
+                wait=tenacity.wait_exponential(),
+                retry=tenacity.retry_if_exception_type(
+                    (ApiExceededException, TokenInvalidException)
+                ),
+            ),
+        )
+
+        result = ig_service.fetch_accounts()
+
+        assert responses.assert_call_count(url, 3) is True
         assert isinstance(result, pd.DataFrame)
         assert result.iloc[0]["accountId"] == "XYZ987"
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,4 +1,9 @@
-from trading_ig.rest import IGService, IGException, ApiExceededException
+from trading_ig.rest import (
+    IGService,
+    IGException,
+    ApiExceededException,
+    TokenInvalidException,
+)
 from trading_ig.config import config
 import pandas as pd
 from datetime import datetime, timedelta
@@ -13,7 +18,8 @@ from tenacity import Retrying, wait_exponential, retry_if_exception_type
 def retrying():
     """test fixture creates a tenacity.Retrying instance"""
     return Retrying(
-        wait=wait_exponential(), retry=retry_if_exception_type(ApiExceededException)
+        wait=wait_exponential(),
+        retry=retry_if_exception_type((ApiExceededException, TokenInvalidException)),
     )
 
 
